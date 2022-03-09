415be72cc18bfea7146eefbbaa420994b38d9423
Christopher Chhouk <cvchhouk@gmail.com>
2022-03-08 23:10:10 -0800

    second commit

---
+++ js/ex8.js
@@ -0,0 +1,17 @@
+console.log(document.body.childNodes[0]); //#text
+console.log("====================")
+
+
+console.log(document.body.childNodes[1]); //h1
+console.log("====================")
+
+
+console.log(document.body.childNodes[20]); //undefined
+console.log("====================")
+
+
+console.log(document.body.childNodes[5].childNodes[0]); //#text
+console.log("====================")
+
+
+console.log(document.body.childNodes[5].childNodes[1]); //li
